7af32adb6b994c3ced25bdc0bb0abf84cadc2c57
Pawel Cala <pawelcala@gmail.com>
2017-04-15 10:50:24 +0200

    ios strings import

--- stringify.py
+++ stringify.py
@@ -74,13 +74,13 @@ class GoogleDocDataLoader(DataLoader):
 
 
 class AndroidStringsLoader(DataLoader):
-    def __init__(self, **kwargs):
-        self.path = kwargs['path']
-        self.xml_name = kwargs['xml_name'] if 'xml_name' in kwargs.keys() else 'strings.xml'
+    def __init__(self, path, **kwargs):
+        self.path = path
+        self.filename = kwargs['xml_name'] if 'xml_name' in kwargs.keys() else 'strings.xml'
         self.default_language = kwargs['default'] if 'default' in kwargs.keys() else 'en'
 
     def load(self):
-        file_paths = find_files(path=self.path, filename_regex=self.xml_name)
+        file_paths = find_files(path=self.path, filename_regex=self.filename)
         dictionary = Dictionary()
         for filepath in file_paths:
             language = self._decode_filepath_language(filepath)
@@ -114,8 +114,42 @@ class AndroidStringsLoader(DataLoader):
 
 
 class IOSStringsLoader(DataLoader):
+    def __init__(self, path, **kwargs):
+        self.path = path
+        self.filename = kwargs['filename'] if 'filename' in kwargs.keys() else 'Localizable.strings'
+
     def load(self):
-        pass
+        filepaths = find_files(path=self.path, filename_regex=self.filename)
+        dictionary = Dictionary()
+
+        for filepath in filepaths:
+            language = self._decode_filepath_language(filepath)
+            entries = self._decode_file_entries(filepath)
+
+            for key, word in entries:
+                dictionary.add_translation(key, language, word)
+
+        return dictionary
+
+    def _decode_filepath_language(self, path):
+        match = re.match(r"""(.*)\.lproj""", path)
+        if match:
+            prefix = match.group(1)
+            if len(prefix) > 2:
+                prefix = prefix[-2:]
+            return prefix
+        else:
+            raise NotFoundException
+
+    def _decode_file_entries(self, filepath):
+        entries = []
+        file = open(filepath)
+
+        for line in file:
+            match = re.match(r'"(.*)".*=.*"(.*)";', line)
+            if match:
+                entries.append((match.group(1), match.group(2)))
+        return entries
 
 
 # PRODUCERS
